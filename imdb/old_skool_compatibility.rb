03ba7b279c046f55ca8143dafec53a4c38b50dca
Jim Meyer <jim@geekdaily.org>
2009-03-29 11:23:36 -0700

    Refactor to use the class/subclass pattern while maintaining backward compatibility

---
+++ imdb/old_skool_compatibility.rb
@@ -0,0 +1,16 @@
+class Imdb < IMDB
+end
+
+class ImbdCompany < IMDB::Company
+end
+
+class ImdbGenre < IMDB::Genre
+end
+
+class ImdbMovie < IMDB::Movie
+end
+
+class ImdbName < IMDB::Name
+end
+
+  
