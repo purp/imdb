c39364b77784b5c2edf36a9a230df4351fd8a19c
Jim Meyer <jim@geekdaily.org>
2009-04-01 12:04:47 -0700

    Move libraries into lib dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..')
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 FIXTURE_DIR = File.join(File.dirname(__FILE__), 'fixtures')
 
 # Stolen from Facets http://facets.rubyforge.org/
